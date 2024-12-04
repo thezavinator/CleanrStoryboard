a0e78f2dd3fad03c62adf31ac714d8858fd1bfc1
thezavinator <thezave@gmail.com>
2024-12-04 12:23:15 -0600

    Initial Commit

---
+++ CleanrStoryboard/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CleanrStoryboard
+//
+//  Created by Zavier Hofstetter on 12/4/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
